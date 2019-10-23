0c05074d9402c3ef02a0d5d01ac41a5a868d2ee0
rikkyps <rikky.putra14@gmail.com>
2019-10-23 11:22:47 +0700

    initial commit

---
+++ json/latihan1.php
@@ -0,0 +1,18 @@
+<?php
+  $mahasiswa = 
+  [
+    [
+      "nama" => "Riky Permana", 
+      "Nim" => "2013112008",
+      "email" => "rikky.putra14@mail.com"
+    ],
+    [
+      "nama" => "Riky Permana", 
+      "Nim" => "2013112008",
+      "email" => "rikky.putra14@mail.com"
+    ]
+  ];
+
+echo json_encode($mahasiswa);
+
+?>
