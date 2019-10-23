235c59f8b7d84257ce9f3a2f0e725585161fbb15
rikkyps <rikky.putra14@gmail.com>
2019-10-23 20:56:39 +0700

    Menambahkan menu.json dan ditampilkan ke page menggunakan php

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,18 +1,24 @@
 <?php
-  $mahasiswa = 
-  [
-    [
-      "nama" => "Riky Permana", 
-      "Nim" => "2013112008",
-      "email" => "rikky.putra14@mail.com"
-    ],
-    [
-      "nama" => "Riky Permana", 
-      "Nim" => "2013112008",
-      "email" => "rikky.putra14@mail.com"
-    ]
-  ];
+  // $mahasiswa = 
+  // [
+  //   [
+  //     "nama" => "Riky Permana", 
+  //     "Nim" => "2013112008",
+  //     "email" => "rikky.putra14@mail.com"
+  //   ],
+  //   [
+  //     "nama" => "Riky Permana", 
+  //     "Nim" => "2013112008",
+  //     "email" => "rikky.putra14@mail.com"
+  //   ]
+  // ];
 
-echo json_encode($mahasiswa);
+$dbh = new PDO('mysql:host=localhost;dbname=db_laravelapp', 'root');
+$db = $dbh->prepare('SELECT * FROM siswa');
+$db->execute();
+$siswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+$data =  json_encode($siswa);
+echo $data;
 
 ?>
